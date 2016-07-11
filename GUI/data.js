60a5dcb5d2f9b101e88335a6d0e70ee5b1496aed
Eridy Lukau <info.lukau@gmail.com>
2016-07-11 19:33:46 +0200

    - added dummy data in data.js - spelling

---
+++ GUI/data.js
@@ -0,0 +1,30 @@
+var DATA = {"a" : "b", "c" : "d"};
+
+var RESPONSE = {
+  "name": "alex",
+  "vendor": "OpenPolicy",
+  "version": "0.1",
+  "success": "true/false",
+  "locations": [
+    {
+      "name": "Strato",
+      "country": "Germany",
+      "price": "2€"
+    },
+    {
+      "name": "dsakdkvd",
+      "country": "Germany",
+      "price": "1€"
+    },
+    {
+      "name": "Blub",
+      "country": "Frankreich",
+      "price": "1€"
+    },
+    {
+      "name": "2kjd",
+      "country": "Great Britain",
+      "price": "2€"
+    }
+]
+}
